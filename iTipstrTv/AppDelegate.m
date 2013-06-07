edeae45f1064ebec91305514f72f784cea451c6c
Leads Mobile App Team <rubaeadtanverferdous@ip191c200.banglalionwimax.com>
2013-06-07 11:33:15 +0600

    only a line has been added

--- iTipstrTv/AppDelegate.m
+++ iTipstrTv/AppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    // a new line has been added.
     return YES;
 }
 							
